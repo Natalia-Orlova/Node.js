f6959eadf9c789b5f38dc79ac3bee117d86b3f19
NataliaOrlova <natakarelskaya@yandex.ru>
2023-12-17 20:03:03 +0400

    added main, about, 404 pages

--- homework1/index.js
+++ homework1/index.js
@@ -9,6 +9,26 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     console.log('Запрос получен');
+    switch (req.url) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+            res.end(`
+            <h1>Добро пожаловать</h1>
+            <a href='/about'>Обо мне</a>`);
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+            res.end(`
+            <h1>Добро пожаловать на страницу обо мне</h1>
+            <a href='/'>Главная</a>`);
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/html; charset=UTF-8'});
+            res.end(`
+            <h1>Ошибка 404</h1>
+            <a href='/'>Вернуться на главную</a>`);
+            break;
+    }
 });
 const port = 3000;
 server.listen(port, () => {
