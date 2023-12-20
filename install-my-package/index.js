d6737fd2d34e1bda07666d002c0516598869a988
NataliaOrlova <natakarelskaya@yandex.ru>
2023-12-20 19:04:46 +0400

    installed my lib

---
+++ install-my-package/index.js
@@ -0,0 +1,11 @@
+const myCalc = require('natalia-calculator');
+
+const a = 10;
+const b = 2;
+console.log(`${a} + ${b} = ${myCalc.add(a, b)}`);
+console.log(`${a} - ${b} = ${myCalc.sub(a, b)}`);
+console.log(`${a} * ${b} = ${myCalc.mult(a, b)}`);
+console.log(`${a} / ${b} = ${myCalc.div(a, b)}`);
+console.log(`${a} ^ ${b} = ${myCalc.pow(a, b)}`);
+
+
